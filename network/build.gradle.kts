5cab88a20042b5686a0673640d2f5cb33ced8fe3
mdana90 <muhammaddana90@gmail.com>
2024-11-14 22:15:45 +0700

    Create network module

---
+++ network/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("com.android.library")
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.dana.githubuser.network"
+    compileSdk = rootProject.extra.get("compileSdk") as Int
+
+    defaultConfig {
+        minSdk = rootProject.extra.get("minSdk") as Int
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = rootProject.extra.get("javaVersion") as JavaVersion
+        targetCompatibility = rootProject.extra.get("javaVersion") as JavaVersion
+    }
+    kotlinOptions {
+        jvmTarget = rootProject.extra.get("kotlinJvmTarget") as String
+    }
+}
+
+dependencies {
+    implementation(libs.kotlinx.serialization.json)
+
+    // Retrofit
+    implementation(libs.retrofit2.kotlinx.serialization.converter)
+    implementation(libs.okhttp)
+    implementation(libs.retrofit)
+}
