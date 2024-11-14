da759e52e29b25bafa3086719302e135855e5ff7
mdana90 <muhammaddana90@gmail.com>
2024-11-14 20:26:38 +0700

    Init

---
+++ app/build.gradle.kts
@@ -0,0 +1,69 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.dana.githubuser"
+    compileSdk = rootProject.extra.get("compileSdk") as Int
+
+    defaultConfig {
+        applicationId = "com.dana.githubuser"
+        minSdk = rootProject.extra.get("minSdk") as Int
+        targetSdk = rootProject.extra.get("targetSdk") as Int
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = rootProject.extra.get("javaVersion") as JavaVersion
+        targetCompatibility = rootProject.extra.get("javaVersion") as JavaVersion
+    }
+    kotlinOptions {
+        jvmTarget = rootProject.extra.get("kotlinJvmTarget") as String
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.androidxComposeCompiler.get()
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+}
+
+extra.apply {
+    set("javaVersion", JavaVersion.VERSION_17)
+    set("kotlinJvmTarget", "17")
+    set("minSdk", 26)
+    set("targetSdk", 35)
+    set("compileSdk", 35)
+}
