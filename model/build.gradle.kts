0c1ef78413234289a69c0149c018ddc1faeef11f
mdana90 <muhammaddana90@gmail.com>
2024-11-16 08:16:40 +0700

    Create model module

---
+++ model/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.dana.githubuser.model"
+    compileSdk = rootProject.extra.get("compileSdk") as Int
+
+    defaultConfig {
+        minSdk = rootProject.extra.get("minSdk") as Int
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = rootProject.extra.get("javaVersion") as JavaVersion
+        targetCompatibility = rootProject.extra.get("javaVersion") as JavaVersion
+    }
+    kotlinOptions {
+        jvmTarget = rootProject.extra.get("kotlinJvmTarget") as String
+    }
+}
+
+dependencies {
+    testImplementation(libs.junit)
+}
