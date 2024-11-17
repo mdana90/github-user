e4dca759537ec9ff4dcc9db2d53e9c1be089a550
mdana90 <muhammaddana90@gmail.com>
2024-11-17 12:23:09 +0700

    Create ui composables module

---
+++ ui/composables/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.dana.github.composables"
+    compileSdk = rootProject.extra.get("compileSdk") as Int
+
+    defaultConfig {
+        minSdk = rootProject.extra.get("minSdk") as Int
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.androidxComposeCompiler.get()
+    }
+    compileOptions {
+        sourceCompatibility = rootProject.extra.get("javaVersion") as JavaVersion
+        targetCompatibility = rootProject.extra.get("javaVersion") as JavaVersion
+    }
+    kotlinOptions {
+        jvmTarget = rootProject.extra.get("kotlinJvmTarget") as String
+    }
+}
+
+dependencies {
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+
+    implementation(libs.landscapist.coil)
+    implementation(libs.landscapist.placeholder)
+
+}
