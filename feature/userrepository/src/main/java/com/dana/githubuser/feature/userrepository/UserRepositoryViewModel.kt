533ef1b1695f1b44d9e553ed7f321ffb346e006f
mdana90 <muhammaddana90@gmail.com>
2024-11-18 08:50:58 +0700

    Implement load repositories

--- feature/userrepository/src/main/java/com/dana/githubuser/feature/userrepository/UserRepositoryViewModel.kt
+++ feature/userrepository/src/main/java/com/dana/githubuser/feature/userrepository/UserRepositoryViewModel.kt
@@ -1,11 +1,16 @@
 package com.dana.githubuser.feature.userrepository
 
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.dana.githubuser.data.repository.UserRepository
+import com.dana.githubuser.feature.userrepository.repositorylist.RepositoryUIState
+import com.dana.githubuser.feature.userrepository.repositorylist.toUIState
+import com.dana.githubuser.feature.userrepository.userprofile.UserProfileUIState
+import com.dana.githubuser.model.Repository
 import com.dana.githubuser.model.Result
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -19,22 +24,59 @@ class UserRepositoryViewModel @Inject constructor(
 ) : ViewModel() {
     var userProfileUIState by mutableStateOf<UserProfileUIState>(UserProfileUIState.Loading)
         private set
+    var isLoadingRepositories by mutableStateOf(false)
+        private set
+
+    private val _repositories = mutableStateListOf<RepositoryUIState>()
+    val repositories: List<RepositoryUIState> = _repositories
+
+    private val username = "mojombo"
+    private var currentPage = 1
+    private var canLoadRepositories = false
 
     fun load() {
         viewModelScope.launch {
             val result = withContext(Dispatchers.IO) {
-                userRepository.getUser("mojombo")
+                userRepository.getUser(username)
             }
 
             when (result) {
                 is Result.Success -> {
                     userProfileUIState = UserProfileUIState.Success(result.data)
-                    println("username: ${result.data.username}")
+                    canLoadRepositories = true
+                    loadRepositories()
                 }
                 is Result.Error -> {
                     println("error: ${result.exception}")
                 }
             }
         }
+    }
+
+    fun loadRepositories() {
+        if (!canLoadRepositories || isLoadingRepositories) return
+
+        isLoadingRepositories = true
+        viewModelScope.launch {
+            val result = withContext(Dispatchers.IO) {
+                userRepository.getUserRepositories(username, currentPage, PER_PAGE)
+            }
+            when (result) {
+                is Result.Success -> {
+                    if (currentPage == 1) _repositories.clear()
+                    _repositories.addAll(result.data.map(Repository::toUIState))
+                    currentPage++
+                    canLoadRepositories = result.data.isNotEmpty()
+                }
+                is Result.Error -> {
+                    println("error: ${result.exception.message}")
+                }
+            }
+            isLoadingRepositories = false
+        }
+    }
+
+    companion object {
+        private const val PER_PAGE = 20
     }
 }
